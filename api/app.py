ff3f1977bd199e81d30ef15a397703be2f95a9a8
Lauren Clayberg <lauren.clayberg@gmail.com>
2022-01-23 12:58:35 -0500

    Add endpoint definitions for mvp

--- api/app.py
+++ api/app.py
@@ -4,5 +4,88 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def welcome():
+    return 'Welcome to MealPlanner!'
+
+@app.route('/ingredient/create', methods = ['POST'])
+def ingredient_create():
+    if request.method == 'POST':
+        data = request.form
+        # todo
+    else:
+        # todo raise error
+
+@app.route('/ingredient/update/<ingredient_id>', methods = ['GET', 'POST', 'DELETE'])
+def ingredient_update(ingredient_id):
+    if request.method == 'GET':
+        # todo
+    elif request.method == 'POST':
+        data = request.form
+        # todo
+    elif request.method == 'DELETE':
+        # todo
+    else:
+        # todo raise error
+
+@app.route('/ingredient/search/name/<ingredient_name>', methods = ['GET'])
+def ingredient_search_name(ingredient_name):
+    if request.method == 'GET':
+        # todo
+    else:
+        # todo raise error
+
+@app.route('/ingredient/search/all', methods = ['GET'])
+def ingredient_search_all(ingredient_name):
+    ''' Get all of the available ingredients
+    '''
+    if request.method == 'GET':
+        # todo
+    else:
+        # todo raise error
+
+@app.route('/recipe/create', methods = ['POST'])
+def recipe_create():
+    if request.method == 'POST':
+        data = request.form
+        # todo
+    else:
+        # todo raise error
+
+@app.route('/recipe/update/<recipe_id>', methods = ['GET', 'POST', 'DELETE'])
+def recipe_update(recipe_id):
+    if request.method == 'GET':
+        # todo
+    elif request.method == 'POST':
+        data = request.form
+        # todo
+    elif request.method == 'DELETE':
+        # todo
+    else:
+        # todo raise error
+
+@app.route('/recipe/search/name/<recipe_name>', methods = ['GET'])
+def recipe_search_name(recipe_name):
+    if request.method == 'GET':
+        # todo
+    else:
+        # todo raise error
+
+@app.route('/recipe/search/all', methods = ['GET'])
+def recipe_search_all(recipe_name):
+    ''' Get all of the available recipes
+    '''
+    if request.method == 'GET':
+        # todo
+    else:
+        # todo raise error
+
+@app.route('/planner/generate', methods = ['POST'])
+def meal_planner_generate_plan():
+    ''' Returns a meal plan and grocery list based on the
+        supplied criteria
+    '''
+    if request.method == 'POST':
+        data = request.form
+        # todo
+    else:
+        # todo raise error
