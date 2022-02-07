c7ee044e9bda25bb2af4e02084ab3acaaf4b7e1d
Lauren Clayberg <lauren.clayberg@gmail.com>
2022-02-06 21:25:52 -0500

    Specify recipes, fix bug with recipe copies

--- api/planner_utils.py
+++ api/planner_utils.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 '''
 TODO
@@ -13,34 +14,58 @@ class PlannerParams:
 
     def __init__(self):
         # Set meal planner defaults
-        self.dinner_days = 7
-        self.dinner_servings = [2,2,2,2,2,2,2]
+        # TODO - set to real defaults when done with testing
+        self.dinner_servings = [6,2,2,2,8] # list(integer)
+        self.recipe_ids = ["fb19b4eb-6efd-453f-a8ea-2d21e682a7f3", None, None, None, None] # list(string)
 
 def verify_parameters(planner_params):
     ''' Checks to make sure parameters do not conflict
     '''
+    dinner_servings = planner_params.dinner_servings
+    recipe_ids = planner_params.recipe_ids
+    if dinner_servings and recipe_ids and len(dinner_servings) != len(recipe_ids):
+        # If recipes are provided, meal counts need to match between recipes and servings
+        return False
+    elif recipe_ids and not dinner_servings:
+        # For every recipe included, you need to include servings required
+        return False
     return True
 
 def generate_meal_plan(recipes, planner_params):
     ''' Generate a meal plan
 
         This meal plan generator has support for the following parameters:
-        - dinner_days (integer)
         - dinner_servings (list(integer))
 
         Returns: a list of formatted recipes
     '''
+    count_chosen_recipes = sum([int(x != None) for x in planner_params.recipe_ids])
+
     # Choose recipes for the meal plan
-    count_meal_plan_recipes = planner_params.dinner_days
+    count_meal_plan_recipes = len(planner_params.dinner_servings) - count_chosen_recipes
     count_database_recipes = len(recipes)
     selection_range = max(count_meal_plan_recipes, count_database_recipes)
 
     recipe_selection = random.sample(range(selection_range), count_meal_plan_recipes)
     recipe_selection = [num % count_database_recipes for num in recipe_selection]
 
+    # Add in pre-chosen recipes
+    other_recipes = [copy.deepcopy(recipes[i]) for i in recipe_selection]
+    chosen_recipes = []
+    idx = 0
+    for recipe_id in planner_params.recipe_ids:
+        if recipe_id != None:
+            for recipe in recipes:
+                if recipe_id == recipe['id']:
+                    chosen_recipes.append(copy.deepcopy(recipe))
+                    break
+        else:
+            chosen_recipes.append(other_recipes[idx])
+            idx += 1
+
     # Convert recipes to correct servings per meal
-    for i in range(len(recipes)):
-        recipe = recipes[i]
+    for i in range(len(chosen_recipes)):
+        recipe = chosen_recipes[i]
         servings = planner_params.dinner_servings[i]
         multiplier = float(servings) / float(recipe['servings'])
 
@@ -48,7 +73,7 @@ def generate_meal_plan(recipes, planner_params):
         for ingredient in recipe['ingredients']:
             ingredient['quantity'] = multiplier * float(ingredient['quantity'])
 
-    return [recipes[i] for i in recipe_selection]
+    return chosen_recipes
 
 def generate_grocery_list(recipes, ingredient_locations):
     ''' Create a grocery list from a list of recipes
